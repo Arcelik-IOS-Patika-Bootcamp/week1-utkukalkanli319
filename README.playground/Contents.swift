312246acad172dc7c5c3aaaa8f205d52d9f205e5
utkukalkanli319 <kalkanliutku@gmail.com>
2022-05-20 21:17:20 +0300

    part-1 struct

--- README.playground/Contents.swift
+++ README.playground/Contents.swift
@@ -1,5 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Week 1 Assignment"
 
-print(3+5)
+// PART 1-STRUCT
+// this is how we define a struct in swift
+struct Person {
+
+  var name = " "
+  var age = 0
+  func sayMyName(){
+      print("My name is \(name)")
+  }
+}
+
+// create instance of struct
+var person1 = Person()
+print(person1)
+
+// now lets change the properties of that instance
+person1.name = "swifty"
+person1.age = 34
+print(person1)
+// call struct function
+person1.sayMyName()
+
+
+
